55e355726d3c8e06dde72b1c68dfca8b55f095b0
SomaTherion <nocturne_wof@hotmail.es>
2020-01-20 19:43:29 +0100

    primer commit

--- models/modelo.py
+++ models/modelo.py
@@ -3,13 +3,14 @@ from odoo import models, fields, api
 class Modelo(models.Model):
     _name = 'calculo.modelo'
     nombre = fields.Char('Articulo', required=True)
-    precio = fields.Integer('Precio', required=True)
+    precio = fields.Float('Precio', required=True)
     iva = fields.Integer('IVA', required=True)
     cantidad = fields.Integer('Cantidad', required=True)
     total = fields.Float(string='Total', compute='_total')
 
+
     @api.one
     @api.depends('precio', 'cantidad', 'iva')
     def _total(self):
-        self.total = self.precio * self.cantidad * (1+","+self.cantidad)
+        self.total = self.precio * self.cantidad * ((self.cantidad/100)+1)
 

371cb7307074a439ef50882ce30feafb252d7db5
SomaTherion <nocturne_wof@hotmail.es>
2020-01-24 16:34:37 +0100

    primer commit

--- models/modelo.py
+++ models/modelo.py
@@ -8,15 +8,15 @@ class Modelo(models.Model):
     desglose = fields.Float('Desglose', compute='_iva')
     cantidad = fields.Integer('Cantidad', required=True)
     total = fields.Float(string='Total', compute='_total')
-    totalIva = fields.Float(string='TotalIVA', compute='_total')
+    totalIva = fields.Float(string='TotalIVA', compute='_sumaTotal')
 
 
     @api.one
-    @api.depends('precio', 'cantidad', 'iva')
+    @api.depends('precio', 'cantidad')
     def _total(self):
-        self.total = self.precio * self.cantidad * ((self.iva/100)+1)
+        self.total = self.precio * self.cantidad
 
-     @api.depends('precio', 'cantidad', 'iva')
+    @api.depends('precio', 'cantidad', 'iva')
     def _iva(self):
             self.total = self.precio - (self.precio * self.cantidad * ((self.iva / 100) + 1))
 
